44c35bf0d8faaa973ca93c4d8de1dec0e37502ee
Aether <64797587+Aeastr@users.noreply.github.com>
2025-11-18 21:29:01 +0000

    Simplify playground app and update README

--- Playground/GarnishPlayground/GarnishPlaygroundApp.swift
+++ Playground/GarnishPlayground/GarnishPlaygroundApp.swift
@@ -7,19 +7,12 @@
 
 import SwiftUI
 import Garnish
-import GarnishTheme
+
 @main
 struct GarnishPlaygroundApp: App {
     var body: some Scene {
         WindowGroup {
-            TabView {
-                Tab("Base", systemImage: "paintpalette") {
-                    GarnishDemoApp()
-                }
-                Tab("Theme", systemImage: "wand.and.stars") {
-                    GarnishThemeDemoApp()
-                }
-            }
+            GarnishDemoApp()
         }
     }
 }
